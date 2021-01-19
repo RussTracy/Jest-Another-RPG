7a66ef6fa2d99a3b27cf12253fcc7e22a5e63d2b
Russ <ocjade@comcast.net>
2021-01-19 10:16:52 -0700

    Potion Object

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,18 @@
+const { TestScheduler } = require('jest');
+const Potion = require('../lib/Potion.js');
+
+test('Creates a health potion object', () => {
+    const potion = new Potion('Health');
+
+    expect(potion.name).toBe('Health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('Creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+
+});
